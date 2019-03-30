547d278973d24ffd25a0123203633fd53fa499b3
angel112 <ngllakra@gmail.com>
2019-03-30 09:48:33 +0530

    Create Login.py

---
+++ Login.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome('C:\Program Files (x86)\chromedriver.exe')
+
+driver.get('https://172.16.1.1:8090/')
+
+user = "  """Enter Your Username Here"""  "
+passw = " """Enter Your Password Here""" "
+
+a = driver.find_element_by_xpath("//input[@type = 'text']")
+a.send_keys(user)
+
+b = driver.find_element_by_xpath("//input[@type = 'password']")
+b.send_keys(passw)
+
+driver.find_element_by_xpath("//input[@type = 'submit']").click()
+
+driver.close()
+
+
+
+                
